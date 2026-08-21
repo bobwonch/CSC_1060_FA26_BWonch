4c66c4f34f2dd29fb53d1f6e09139c26b5811bd7
Wonch <Robert.Wonch@frontrange.edu>
2026-08-20 18:26:34 -0600

    First Push

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author S01881046
+ * A simple hello world app
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World from BP119!");
+	}
+
+}
