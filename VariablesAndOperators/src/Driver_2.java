c59f81f752da1fe3c0397e20fbc7d5cd87038926
Wonch <Robert.Wonch@frontrange.edu>
2026-08-31 10:27:00 -0600

    Vars And Operators

---
+++ VariablesAndOperators/src/Driver_2.java
@@ -0,0 +1,36 @@
+
+public class Driver_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message;
+		int num;
+		float fPoint;
+		double dPoint;
+		message = "Hello World";
+		num = 3;
+		fPoint = 1.36f;
+		dPoint = 1.36;
+		
+		System.out.println(message + num + fPoint);
+		
+		int hour = 9;
+		int minute = 59;
+		System.out.println(hour + ":" + minute);
+		
+		System.out.println(0.1 * 10);
+		System.out.println(.1 + .1 + .1 + .1 
+				+ .1 + .1 + .1 + .1 + .1 + .1);
+		System.out.println(3/1);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
