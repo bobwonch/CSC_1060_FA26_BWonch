580d1fe4c92b1f837f907c00866b72a716fa9705
robert wonch <bobwonch@gmail.com>
2026-09-07 05:39:03 -0600

    Added a Sandbox

--- Assignments/src/GasPump.java
+++ Assignments/src/GasPump.java
@@ -18,83 +18,83 @@ public class GasPump {
 		
 		//Asks if the customer is a loyalty card holder
 		
-//		System.out.println(LINE);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		
-//		System.out.printf("*%53s %23s", "Are you a loyalty card holder? y/n", "*\n");
-//		
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.println(LINE);
-//		
-//		card = input.next();
-//		
-//		System.out.println(PAGE);
-//		
-//		//gets phone number associated with the loyalty card
-//		
-//		System.out.println(LINE);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		
-//		System.out.printf("*%48s %28s", "Enter your phone number", "*\n");
-//		
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.println(LINE);
-//		
-//		phone = input.next();
-//		
-//		System.out.println(PAGE);
-//		
-//		//Gets the grade of the gas regular, mid, premium
-//		
-//		System.out.println(LINE);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		
-//		System.out.printf("*%51s %25s", "Regular, Mid-Grade, or Premium", "*\n");
-//		
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.println(LINE);
-//		
-//		grade = input.next();
-//		
-//		System.out.println(PAGE);
-//		
-//	//Gets the grade of the gas regular, mid, premium
-//		
-//		System.out.println(LINE);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		
-//		System.out.printf("*%51s %25s", "How many gallons?", "*\n");
-//				
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.printf(WALL);
-//		System.out.println(LINE);
-//		
-//		gallons = input.nextDouble();
-//		
-//		System.out.println(PAGE);
+		System.out.println(LINE);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		
+		System.out.printf("*%53s %23s", "Are you a loyalty card holder? y/n", "*\n");
+		
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.println(LINE);
+		
+		card = input.next();
+		
+		System.out.println(PAGE);
+		
+		//gets phone number associated with the loyalty card
+		
+		System.out.println(LINE);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		
+		System.out.printf("*%48s %28s", "Enter your phone number", "*\n");
+		
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.println(LINE);
+		
+		phone = input.next();
+		
+		System.out.println(PAGE);
+		
+		//Gets the grade of the gas regular, mid, premium
+		
+		System.out.println(LINE);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		
+		System.out.printf("*%51s %25s", "Regular, Mid-Grade, or Premium", "*\n");
+		
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.println(LINE);
+		
+		grade = input.next();
+		
+		System.out.println(PAGE);
+		
+	//Gets the grade of the gas regular, mid, premium
+		
+		System.out.println(LINE);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		
+		System.out.printf("*%51s %25s", "How many gallons?", "*\n");
+				
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.printf(WALL);
+		System.out.println(LINE);
+		
+		gallons = input.nextDouble();
+		
+		System.out.println(PAGE);
 		
 	//Output
 		grade = "Premium";
